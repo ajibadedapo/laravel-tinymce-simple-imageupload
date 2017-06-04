e84ccf0e6036ada80e4b9d224db3e12cd22dca7b
AJIBADE,DAPO <ajibadehammed@gmail.com>
2017-06-04 08:22:30 +0100

    Update TinymceController.php

--- src/TinymceController.php
+++ src/TinymceController.php
@@ -13,6 +13,25 @@ class TinymceController extends Controller
      * @param  Request $request
      * @return Response
      */
+   
+    function compress($source, $destination, $quality) {
+
+        $info = getimagesize($source);
+
+        if ($info['mime'] == 'image/jpeg')
+            $image = imagecreatefromjpeg($source);
+
+        elseif ($info['mime'] == 'image/gif')
+            $image = imagecreatefromgif($source);
+
+        elseif ($info['mime'] == 'image/png')
+            $image = imagecreatefrompng($source);
+
+        imagejpeg($image, $destination, $quality);
+
+        return $destination;
+    }
+
     public function uploadImage(Request $request)
     {
         $image = $request->file('image');
@@ -20,6 +39,10 @@ class TinymceController extends Controller
         $filename = 'image_'.time().'_'.$image->hashName();
         $image = $image->move(public_path('img'), $filename);
 
+        $source_img = public_path('img').'/'.$filename;
+        $destination_img = public_path('img').'/'.$filename;
+
+        $d = $this->compress($source_img, $destination_img, 40);
         return mce_back($filename);
     }
 
